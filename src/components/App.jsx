d390159fb7057e34302cd685452a35d040206351
Renato Gonçalves <renatoslip@hotmail.com>
2021-09-10 10:09:43 -0300

    commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Components
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>React - from Angela Yu</h1>
+      <p>This is my Firs contact with React.js :D</p>
+    </div>
+  );
+}
